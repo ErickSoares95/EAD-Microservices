c2fb198c94fcd57c0a9e9148da1fa3c6f45f33d0
Erick de Lira Soares <ericklira99@gmail.com>
2023-08-21 17:55:41 -0300

    add in course models, services, repositories and enums

---
+++ EAD/course/src/main/java/com/ead/course/models/CourseModel.java
@@ -0,0 +1,54 @@
+package com.ead.course.models;
+
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_COURSES")
+
+public class CourseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID courseId;
+    @Column(nullable = false, length = 150)
+    private String name;
+    @Column(nullable = false, length = 250)
+    private String description;
+    @Column
+    private String imageUrl;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseStatus courseStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseLevel courseLevel;
+    @Column(nullable = false)
+    private UUID userInstructor;
+
+    //Set foi utilizado porque o hibernate trabalha melhor com ele, podendo assim trazer varias entidades relacionadas a
+    // curso e também evita problemas de multiplas querys.
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //Campo não mostrado nos gets, apenas escrita
+    @OneToMany(mappedBy = "course")
+    private Set<ModuleModel> modules;
+
+}
